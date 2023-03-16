7de92d375544d1b339bd33df3cce098caae245ae
slizer98 <erick.m.villa.r@gmail.com>
2023-03-15 19:08:49 -0600

    agregando el modelo vacantes

---
+++ models/Vacantes.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+mongoose.Promise = global.Promise;
+import slug from "slug";
+import shortid from "shortid";
+
+const vacantesSchema = new mongoose.Schema({
+    titulo: { 
+        type: String,
+        required: 'El nombre de la vacante es obligatorio',
+        trim: true
+    },
+    empresa: { 
+        type: String,
+        trim: true
+    },
+    ubicacion: {
+        type: String,
+        trim: true,
+        required: 'La ubicación es obligatoria'
+    },
+    salario: {
+        type: String,
+        default: 0,
+        trim: true
+    },
+    contrato: {
+        type: String,
+        trim: true
+    },
+    descripcion: {
+        type: String,
+        trim: true
+    },
+    url: {
+        type: String,
+        lowercase: true
+    },
+    skills: [String],
+    candidatos: [{
+        nombre: String,
+        email: String,
+        cv: String
+    }],
+});
+
+// exportar el modelo
+export default mongoose.model('Vacante', vacantesSchema);
